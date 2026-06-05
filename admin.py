8578264dc1c9f07b2b7ebac22fdecd7f30eb2ec5
JDH777jdh <johen.diazh@gmail.com>
2026-06-04 22:15:54 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -2656,7 +2656,7 @@ if st.button(
                     pdf.set_x(margen_izquierdo)
                     pdf.ln(4.5) 
                     
-                pdf.ln(1)
+                pdf.ln(0)
                 # --- INYECCIÓN DETALLE DE ALERGIAS ---
                 # Obtenemos el detalle (asegúrate que la llave coincida con la de tu BD)
                 detalle_alergia = datos_doc.get('alergias_detalles', '').strip()
